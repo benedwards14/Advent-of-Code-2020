b85654719343a2ef133549c9647fce11afcfec36
benedwar <benedwar@cisco.com>
2020-12-18 22:48:07 +0000

    Refactor Day 18

--- python/day_18.py
+++ python/day_18.py
@@ -1,109 +1,101 @@
 import dataclasses
 import numpy
-from typing import Optional, Tuple, Union
+from typing import List, Optional, Tuple, Union
 
 import utils
 
 
-Parentheses = 'Stmt'
-Value = Union[int, Parentheses]
+@dataclasses.dataclass
+class SyntaxNode:
+    _value: Union[int, 'SyntaxTree']
+
+    def evaluate(self) -> int:
+        if isinstance(self._value, int):
+            return self._value
+        assert isinstance(self._value, SyntaxTree)
+        return self._value.evaluate()
 
 
 @dataclasses.dataclass
-class Operation:
-    op: str
-    value: Value = None
+class SyntaxTree:
+    _node: str
+    _children: List[Union[SyntaxNode, 'SyntaxTree']]
+
+    def add_to(self, equation: Union[SyntaxNode, 'SyntaxTree']):
+        self._children = [equation] + self._children
+        return self
+
+    def evaluate(self) -> int:
+        if self._node == '+':
+            return self._children[0].evaluate() + self._children[1].evaluate()
+        return self._children[0].evaluate() * self._children[1].evaluate()
 
 
-@dataclasses.dataclass
-class Stmt:
-    value: Union[Value, Operation]
-    next: Optional['Stmt'] = None
+class SyntaxTree2(SyntaxTree):
+    def add_to(self, equation: Union[SyntaxNode, SyntaxTree]):
+        if isinstance(equation, SyntaxTree) and self._node == '+':
+            self._children = [equation._children[1]] + self._children
+            equation._children[1] = self
+            return equation
+
+        self._children = [equation] + self._children
+        return self
 
 
-def parse_equation(eq_str: str) -> Tuple[Stmt, str]:
-    equation = None
-    last_stmt = None
-    remaining_str = eq_str
+def parse_equation(eq_str: str) -> Tuple[SyntaxTree, str]:
+    equation, remaining_str = parse_next_operation(eq_str)
 
     while remaining_str:
-        next_value = None
-        next_op = None
+        node, remaining_str = parse_next_operation(
+            remaining_str
+        )
+        if node is None:
+            return equation, remaining_str
+
+        equation = node.add_to(equation)
+
+    return equation, ""
+
+
+def parse_next_operation(eq_str: str) -> Tuple[Optional[SyntaxTree], str]:
+    remaining_str = eq_str
+    child = None
+    op = None
+
+    while child is None:
         next_char, *remaining_str = remaining_str
+
         if next_char.isspace():
             continue
 
         if next_char == '(':
-            next_value, remaining_str = parse_equation(remaining_str)
+            child, remaining_str = parse_equation(remaining_str)
+            child = SyntaxNode(child)
         elif next_char == ')':
-            return equation, remaining_str
+            return None, remaining_str
 
         elif next_char.isdigit():
-            next_value = int(next_char)
+            child = SyntaxNode(int(next_char))
         elif next_char in ['+', '*']:
-            next_op = next_char
+            op = next_char
 
-        if next_op is not None:
-            next_stmt = Stmt(Operation(next_op))
-            last_stmt.next = next_stmt
-            last_stmt = next_stmt
-        else:
-            assert next_value is not None
-            if equation is None:
-                equation = Stmt(next_value)
-                last_stmt = equation
-            else:
-                last_stmt.value.value = next_value
+    if op is None:
+        return child, remaining_str
 
-    return equation, None
-
-
-def get_value(value: Value) -> int:
-    return evaluate_equation(value) if isinstance(value, Stmt) else value
-
-
-def evaluate_equation(equation: Stmt) -> int:
-    total_value = get_value(equation.value)
-    curr_stmt = equation.next
-
-    while curr_stmt is not None:
-        assert isinstance(curr_stmt.value, Operation)
-        next_value = get_value(curr_stmt.value.value)
-        if curr_stmt.value.op == '+':
-            total_value += next_value
-        else:
-            assert curr_stmt.value.op == '*'
-            total_value *= next_value
-        curr_stmt = curr_stmt.next
-
-    return total_value
-
-
-def get_value2(value: Value) -> int:
-    return evaluate_equation2(value) if isinstance(value, Stmt) else value
-
-
-def evaluate_equation2(equation: Stmt) -> int:
-    add_values = [get_value2(equation.value)]
-    curr_stmt = equation.next
-
-    while curr_stmt is not None:
-        assert isinstance(curr_stmt.value, Operation)
-        next_value = get_value2(curr_stmt.value.value)
-        if curr_stmt.value.op == '+':
-            add_values[-1] += next_value
-        else:
-            assert curr_stmt.value.op == '*'
-            add_values.append(next_value)
-        curr_stmt = curr_stmt.next
-
-    return numpy.prod(add_values)
+    return Tree(op, [child,]), remaining_str
 
 
 if __name__ == "__main__":
+    Tree = SyntaxTree
     equations = [
         parse_equation(equation)[0]
         for equation in utils.get_data(18).splitlines()
     ]
-    assert sum(evaluate_equation(equation) for equation in equations) == 75592527415659
-    assert sum(evaluate_equation2(equation) for equation in equations) == 360029542265462
+    assert sum(equation.evaluate() for equation in equations) == 75592527415659
+
+    Tree = SyntaxTree2
+    equations = [
+        parse_equation(equation)[0]
+        for equation in utils.get_data(18).splitlines()
+    ]
+    assert sum(equation.evaluate() for equation in equations) == 360029542265462
