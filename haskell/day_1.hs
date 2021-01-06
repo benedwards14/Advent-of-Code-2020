00c11809226d2101f3f253ecf497d6dd9a3926c4
benedwar <benedwar@cisco.com>
2021-01-06 18:57:48 +0000

    Haskell Day1

--- haskell/day_1.hs
+++ haskell/day_1.hs
@@ -1,3 +1,32 @@
+import qualified System.IO as IO
+import qualified Data.List as List
+
+
+find2020Product :: Integer -> [Integer] -> Integer
+find2020Product combinations =
+    product . head . filter (\numbs -> sum numbs == 2020) . getCombinations combinations
+
+
+getCombinations :: Integer -> [Integer] -> [[Integer]]
+getCombinations 0 numbers = []
+getCombinations 1 numbers = map (\num -> [num]) numbers
+getCombinations n numbers =
+    [ x:y | x <- numbers, y <- curr_combinations, not $ x `elem` y ]
+    where
+        curr_combinations = getCombinations (n-1) numbers
+
+
+part1 :: [Integer] -> Integer
+part1 = find2020Product 2
+
+
+part2 :: [Integer] -> Integer
+part2 = find2020Product 3
+
+
 main :: IO ()
 main = do
-    putStrLn "Hello Day 1!"
+    contents <- IO.readFile "../data/day_1.txt"
+    let numbers = map (\num -> read num :: Integer) $ lines contents
+    putStrLn $ show $ part1 numbers
+    putStrLn $ show $ part2 numbers
